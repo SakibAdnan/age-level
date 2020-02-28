4b53363a93d1271bc074730e10641761936dddc9
Nazmun Sakib <45472472+SakibAdnan@users.noreply.github.com>
2020-02-29 00:24:28 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 var age = document.querySelector('#age');
+var btn = document.querySelector('#btn');
 var result = document.querySelector('#result');
 
-age.addEventListener("keypress", function(){
+btn.addEventListener("click", function(){
     var ageValue = age.value;
-    if( ageValue  <  11){
-        result.innerHTML = `You Are Children`;
-    }
-    else if(ageValue  < 20){
-        result.innerHTML = `You Are Teenager`;
-    }
-    else if(ageValue < 50){ 
-        result.innerHTML = `You Are Young man`;
-    }
-    else if(ageValue < 200){
-        result.innerHTML = `You Are Old `;
-    }
-    else{
-        result.innerHTML = `You Are Alien`;
-    }
+        if( ageValue  <  11){
+            result.innerHTML = `You Are Children`;
+        }
+        else if(ageValue  < 20){
+            result.innerHTML = `You Are Teenager`;
+        }
+        else if(ageValue < 50){ 
+            result.innerHTML = `You Are Young man`;
+        }
+        else if(ageValue < 200){
+            result.innerHTML = `You Are Old `;
+        }
+        else{
+            result.innerHTML = `You Are Alien`;
+        }
+
+
 });
