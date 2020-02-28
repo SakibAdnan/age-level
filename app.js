dd3da754f9133725e972b6fe954e9cbee8d2302d
Nazmun Sakib <wp.coder3@gmail.com>
2020-02-28 23:13:41 +0600

    Complite add level project

---
+++ app.js
@@ -0,0 +1,21 @@
+var age = document.querySelector('#age');
+var result = document.querySelector('#result');
+
+age.addEventListener("keypress", function(){
+    var ageValue = age.value;
+    if( ageValue  <  11){
+        result.innerHTML = `You Are Children`;
+    }
+    else if(ageValue  < 20){
+        result.innerHTML = `You Are Teenager`;
+    }
+    else if(ageValue < 50){ 
+        result.innerHTML = `You Are Young man`;
+    }
+    else if(ageValue < 200){
+        result.innerHTML = `You Are Old `;
+    }
+    else{
+        result.innerHTML = `You Are Alien`;
+    }
+});
